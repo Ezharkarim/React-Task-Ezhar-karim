7d479b1a92666af78360998aefcef0085bede28f
Ezhar <ezharkarim19@gmail.com>
2023-12-29 02:54:00 +0500

    UI changes

--- src/components/customer/card.js
+++ src/components/customer/card.js
@@ -3,26 +3,26 @@ import EditCustomer from "../../models/EditCustomer";
 import RemoveCustomer from "../../models/RemoveCustomer";
 import CardHeader from "../../common/cardHeader";
 
-const Card = ({ isloading, customer}) => {
+const Card = ({ isloading, customer }) => {
   const [showModal, setShowModal] = useState(false);
   const [editModel, setEditModel] = useState(false);
   const [id, setID] = useState(null);
   const [sortBy, setSortBy] = useState("id");
-  
+
   useEffect(() => {
     console.log("ID", id);
   }, [id]);
 
   const handleRemove = (data) => {
     setShowModal(true);
-    setID(data.id)
-    console.log("ID",id);
-  }
+    setID(data.id);
+    console.log("ID", id);
+  };
   const handleEdit = (data) => {
     setEditModel(true);
-    setID(data.id)
-    console.log("ID",id);
-  }
+    setID(data.id);
+    console.log("ID", id);
+  };
 
   const sortedCustomer = customer.slice().sort((a, b) => {
     if (a[sortBy] !== b[sortBy]) {
@@ -34,37 +34,36 @@ const Card = ({ isloading, customer}) => {
     }
     return 0;
   });
-  
+
   return (
     <>
       {isloading ? (
         <h1>Loading.....</h1>
       ) : (
         <>
-        <CardHeader setSortBy= {setSortBy}/>
+          <CardHeader setSortBy={setSortBy} />
           {sortedCustomer?.map((data) => {
             return (
-              
-                <div
+
+              <div
                 key={data.id}
-                className="flex flex-wrap md:flex-grow justify-around  md:justify-between px-2 items-center bg-white h-[200px] md:h-[124px] rounded-[10px] my-8"
+                className=" flex flex-wrap md:flex-grow justify-around mx-1  md:justify-between px-2 items-center bg-white h-[200px] md:h-[124px] rounded-[10px] my-8"
               >
                 <div className=" rounded-[20px] h-[112px]  overflow-hidden ">
                   <img src={data.avatar} alt="img here" />
-                  
                 </div>
                 <div>
-                  <span className=" text-[18px] font-normal">
+                  <span className=" text-[14px] lg:text-[18px] mx-1 font-normal">
                     {`00${data.id}`}
                   </span>
                 </div>
                 <div>
-                  <span className=" text-[#57BC90] text-[18px] font-semibold">
+                  <span className=" text-[#57BC90] text-[14px] mx-1 lg:text-[18px] font-semibold">
                     {`${data.first_name}`}
                   </span>
                 </div>
                 <div>
-                  <span className=" text-[18px] font-normal">{data.email}</span>
+                  <span className=" text-[14px] lg:text-[18px] mx-1 font-normal">{data.email}</span>
                 </div>
                 <div className="flex">
                   <div
@@ -80,25 +79,21 @@ const Card = ({ isloading, customer}) => {
                       <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
                         <div className="relative w-auto my-6 mx-auto max-w-3xl">
                           {/*content*/}
-                          <EditCustomer setEditModel={setEditModel} id={id}/>
+                          <EditCustomer setEditModel={setEditModel} id={id} />
                         </div>
                       </div>
                       <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
                     </>
                   ) : null}
 
-          
-                    <div
-                    onClick={() => handleRemove(data) }
-                   
+                  <div
+                    onClick={() => handleRemove(data)}
                     className="flex justify-center items-center mr-4 bg-[#D80000] bg-opacity-[40%] h-[33px] w-[106px] text-[18px] cursor-pointer"
                   >
                     <span className="  text-[#D80000] rounded-[5px]  font-semibold">
                       Delete
                     </span>
                   </div>
-             
-                 
 
                   {showModal ? (
                     <>
@@ -112,10 +107,8 @@ const Card = ({ isloading, customer}) => {
                   ) : null}
                 </div>
               </div>
-            
-            
             );
-          })} 
+          })}
         </>
       )}
     </>
--- src/models/EditCustomer.js
+++ src/models/EditCustomer.js
@@ -4,30 +4,28 @@ import { useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { editCustomer } from "../redux/reducers/customers";
 
-const EditCustomer = ({setEditModel, id}) => {
+const EditCustomer = ({ setEditModel, id }) => {
   const dispatch = useDispatch();
-  const {customer} = useSelector((state) => state.customer);
-  console.log("AAAAA", customer);
-
+  const { customer } = useSelector((state) => state.customer);
   const existingCustomer = customer?.filter((f) => f.id === id) || [];
 
   const [{ first_name, email, avator } = {}] = existingCustomer;
-  
+
   const [uname, setName] = useState(first_name);
   const [uemail, setEmail] = useState(email);
   const [uavator, setAvatar] = useState(avator);
-  
+
   const handleImageChange = (event) => {
     const file = event.target.files[0];
-  
+
     if (file) {
       const imgname = file.name;
       const reader = new FileReader();
-  
+
       reader.onloadend = () => {
         const img = new Image();
         img.src = reader.result;
-  
+
         img.onload = () => {
           const canvas = document.createElement("canvas");
           const maxSize = Math.max(img.width, img.height);
@@ -49,89 +47,88 @@ const EditCustomer = ({setEditModel, id}) => {
           );
         };
       };
-  
+
       reader.readAsDataURL(file);
     }
   };
-  
+
   const handleEdit = (e) => {
     e.preventDefault();
-    dispatch(editCustomer({
-      id: id,
-      first_name: uname,
-      email: uemail,
-      avator: uavator
-    }))
-    setEditModel(false)
-  }
+    dispatch(
+      editCustomer({
+        id: id,
+        first_name: uname,
+        email: uemail,
+        avator: uavator,
+      })
+    );
 
+    setEditModel(false);
+  };
 
   const imageRef = useRef(null);
   const handleImageClick = () => {
     imageRef.current.click();
   };
   return (
-    
-      <div className="flex flex-col h-[530px] w-[527px] bg-[#FBFCFC] rounded-[20px]">
-        <div
-          className="relative h-[144px] w-full"
-          style={{
-            backgroundImage: `url(${Maskgroup})`,
-          }}
-        >
-          <div  onClick={() => setEditModel(false)}>
-            <span className=" absolute top-5 right-5 h-[20px] w-[20px] text-white">
-              <CloseOutlined />
-            </span>
-          </div>
-          <span className="absolute bottom-5 right-28 font-Recoleta font-semibold text-[#FFFFFF] text-[40px] flex justify-center">
-            Edit Customer
+    <div className="flex flex-col h-[530px] w-[527px] bg-[#FBFCFC] rounded-[20px]">
+      <div
+        className="relative h-[144px] w-full"
+        style={{
+          backgroundImage: `url(${Maskgroup})`,
+        }}
+      >
+        <div onClick={() => setEditModel(false)}>
+          <span className=" absolute top-5 right-5 h-[20px] w-[20px] text-white">
+            <CloseOutlined />
           </span>
         </div>
-        <form className="flex flex-col" onSubmit={handleEdit}>
-          <input
-            className="h-[56] w-[455] m-6 p-2 bg-white border-[1px]"
-            type="text"
-            name="name"
-            onChange={(e) => setName(e.target.value)}
-            placeholder={first_name}
-          />
-
-          <input
-            className="h-[56] w-[455] m-6 p-2 bg-white border-[1px]"
-            placeholder={email}
-            type="email"
-            name="email"
-            onChange={(e) => setEmail(e.target.value)}
-            
-          />
-
-          <div className="flex ">
-            <span
-              className="z-10 m-6 text-[#57BC90] bg-[#FBFCFC] h-10 cursor-pointer"
-              onClick={handleImageClick}
-            >
-              <u>Upload Photo</u>
-            </span>
-            <input
-              className="m-6 absolute"
-              type="file"
-              ref={imageRef}
-              onChange={handleImageChange}
-            />
-          </div>
-
-          <div  className="flex justify-center items-center mt-4" >
-            <button
-              className="bg-gradient-to-r from-[#57BC90] to-[#004B40] w-[300px] h-[50px]
-             text-white rounded-[10px] flex items-center justify-center text-[16px] cursor-pointer"
-            >
-              <span>EDIT CUSTOMER</span>
-            </button>
-          </div>
-        </form>
+        <span className="absolute bottom-5 right-28 font-Recoleta font-semibold text-[#FFFFFF] text-[40px] flex justify-center">
+          Edit Customer
+        </span>
       </div>
+      <form className="flex flex-col" onSubmit={handleEdit}>
+        <input
+          className="h-[56] w-[455] m-6 p-2 bg-white border-[1px]"
+          type="text"
+          name="name"
+          onChange={(e) => setName(e.target.value)}
+          placeholder={first_name}
+        />
 
+        <input
+          className="h-[56] w-[455] m-6 p-2 bg-white border-[1px]"
+          placeholder={email}
+          type="email"
+          name="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <div className="flex ">
+          <span
+            className="z-10 m-6 text-[#57BC90] bg-[#FBFCFC] h-10 cursor-pointer"
+            onClick={handleImageClick}
+          >
+            <u>Upload Photo</u>
+          </span>
+          <input
+            className="m-6 absolute"
+            type="file"
+            ref={imageRef}
+            onChange={handleImageChange}
+          />
+        </div>
+
+        <div className="flex justify-center items-center mt-4">
+          <button
+            className="bg-gradient-to-r from-[#57BC90] to-[#004B40] w-[300px] h-[50px]
+             text-white rounded-[10px] flex items-center justify-center text-[16px] cursor-pointer"
+          >
+            <span>EDIT CUSTOMER</span>
+          </button>
+        </div>
+      </form>
+    </div>
   );
 };
 
